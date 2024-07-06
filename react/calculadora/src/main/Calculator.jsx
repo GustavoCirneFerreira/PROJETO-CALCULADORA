37a6e21783ac4c24eac0dc0964f19822472c6e6d
Gustavo Cirne Ferreira <114815870+GustavoCirneFerreira@users.noreply.github.com>
2024-07-05 21:05:40 -0300

    Implementações novas no projeto (Comportamento dos botões)

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -4,12 +4,33 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default class Calculator extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    clearMemory() {
+        console.log('LIMPAR')
+    }
+
+    setOperation(operation) {
+        console.log(operation)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+
     render() {
+
         return (
             <div className='calculator'>
                 <Display value={100} />
-                <Button label='AC'/>
-                <Button label='/'/>
+                <Button label='AC' click={this.clearMemory} triple/>
+                <Button label='/' click={this.setOperation} operation/>
                 <Button label='7'/>
                 <Button label='8'/>
                 <Button label='9'/>
